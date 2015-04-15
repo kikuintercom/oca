dbf9e7d470a97037ffb31eba5f510b9a1ed33c4c
Callum McLean <calmcl1@aol.com>
2015-04-15 02:48:45 +0000

    Added all Library datatype forward declarations

--- include/types/librarytypes.h
+++ include/types/librarytypes.h
@@ -5,4 +5,84 @@
 LIBRARY DATATYPES
 ************************************************/
 
+enum class OcaLibVolType : OcaEnumItem
+{
+    None = 0,
+    ParamSet,
+    Patch
+};
+
+typedef OcaUint32 OcaLibVolID;
+
+class OcaLibVolIdentifier
+{
+    OcaONo Library;
+    OcaLibVolID ID;
+
+public:
+    OcaLibVolIdentifier(){};
+    virtual ~OcaLibVolIdentifier(){};
+};
+
+enum class OcaLibAccess : OcaEnumItem
+{
+    None = 0,
+    ReadOnly,
+    ReadExpand,
+    Full
+};
+
+class OcaLibVolMetadata
+{
+    OcaString Name;
+    OcaLibVolType Type;
+    OcaLibAccess Access;
+    OcaUint32 Version;
+    OcaString Creator;
+    OcaTimeOfDay UpDate;
+
+public:
+    OcaLibVolMetadata(){};
+    virtual ~OcaLibVolMetadata(){};
+};
+
+template <class T> class OcaLibVol
+{
+    OcaLibVolMetadata Metadata;
+    T Data;
+
+public:
+    OcaLibVol(){};
+    virtual ~OcaLibVol(){};
+};
+
+class OcaLibParamSetAssignment
+{
+    OcaLibVolIdentifier ParamSetIdentifier;
+    OcaONo TargetBlockNo;
+
+public:
+    OcaLibParamSetAssignment(){};
+    virtual ~OcaLibParamSetAssignment(){};
+};
+
+class OcaLibVolData_ParamSet
+{
+    OcaONo TargetBlockType;
+    OcaBlob ParData;
+
+public:
+    OcaLibVolData_ParamSet(){};
+    virtual ~OcaLibVolData_ParamSet(){};
+};
+
+class OcaLibVolData_Patch
+{
+    OcaList<OcaLibParamSetAssignment> Assignments;
+
+public:
+    OcaLibVolData_Patch(){};
+    virtual ~OcaLibVolData_Patch(){};
+};
+
 #endif // LIBRARYTYPES_H
