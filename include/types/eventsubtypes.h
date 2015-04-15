b1b6b5c4ce16698d241836a438f4bafa979ce3d6
Callum McLean <calmcl1@aol.com>
2015-04-15 00:44:51 +0000

    Added forward declarations for event and subscription datatypes.

--- include/types/eventsubtypes.h
+++ include/types/eventsubtypes.h
@@ -6,20 +6,115 @@ EVENT & SUBSCRIPTION DATATYPES
 ************************************************/
 
 class OcaEvent{
+    OcaONo EmitterONo;
+    OcaEventID EventID;
+
 public:
     OcaEvent(){};
     virtual ~OcaEvent(){};
 };
 
-template <class T>
-class OcaPropertyChangedEventData{
+class OcaMethod
+{
+    OcaONo ONo;
+    OcaMethodID MethodID;
+
 public:
-    uint16_t PropertyID;
+    OcaMethod(){};
+    virtual ~OcaMethod(){};
+};
+
+class OcaEventData
+{
+    OcaEvent Event;
+
+public:
+    OcaEventData(){};
+    virtual ~OcaEventData(){};
+};
+
+class OcaObjectListEventData: public OcaEventData
+{
+    OcaList<OcaONo> objectList;
+
+public:
+    OcaObjectListEventData(){};
+    virtual ~OcaObjectListEventData(){};
+};
+
+enum class OcaPropertyChangeType : OcaEnumItem
+{
+    CurrentChanged = 1,
+    MinChanged,
+    MaxChanged,
+    ItemAdded,
+    ItemChanged,
+    ItemDeleted
+};
+
+template <class T> class OcaPropertyChangedEventData : OcaEventData
+{
+    OcaPropertyID PropertyID;
     T PropertyValue;
-    uint16_t ChangeType;
+    OcaPropertyChangeType ChangeType;
     T PropertyKey;
-// TODO figure this out
+
+public:
     OcaPropertyChangedEventData(){};
+    virtual ~OcaPropertyChangedEventData(){};
+};
+
+class OcaObservationEventData
+{
+    OcaEvent Event;
+    OcaFloat64 Reading;
+
+public:
+    OcaObservationEventData(){};
+    virtual ~OcaObservationEventData(){};
+};
+
+class OcaObservationListEventData
+{
+    OcaEvent Event;
+    OcaList<OcaFloat64> Reading;
+
+public:
+    OcaObservationListEventData(){};
+    virtual ~OcaObservationListEventData(){};
+};
+
+enum class OcaGrouperStatusChangeType : OcaEnumItem
+{
+    citizenAdded = 1,
+    citizenDeleted,
+    citizenConnectionLost,
+    citizenConnectionReEstablished,
+    citizenError,
+    enrollment,
+    unEnrollment
+};
+
+class OcaGrouperStatusChangeEventData : OcaEventData
+{
+    OcaUint16 groupIndex, citizenIndex;
+    OcaGrouperStatusChangeType changeType;
+
+public:
+    OcaGrouperStatusChangeEventData(){};
+    virtual ~OcaGrouperStatusChangeEventData(){};
+};
+
+enum class OcaNotificationDeliveryMode : OcaEnumItem
+{
+    Reliable = 1,
+    Fast
+};
+
+enum class OcaSubscriptionManagerState : OcaEnumItem
+{
+    Normal = 1,
+    EventsDisabled
 };
 
 #endif // EVENTSUBTYPES_H
