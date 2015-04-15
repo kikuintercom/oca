87297b3cd72decadd80ce107b856751b06d49896
Callum McLean <calmcl1@aol.com>
2015-04-15 18:33:10 +0000

    Added forward declarations for Sampling datatypes

--- include/types/samplingtypes.h
+++ include/types/samplingtypes.h
@@ -5,4 +5,33 @@
 SAMPLING DATATYPES
 ************************************************/
 
+enum class OcaMediaClockLockState : OcaEnumItem
+{
+    Undefined = 0,
+    Locked,
+    Synchronizing,
+    FreeRun,
+    Stopped
+};
+
+class OcaMediaClockRate
+{
+    OcaFrequency NominalRate;
+    OcaFrequency PullRange;
+    OcaFloat32 Accuracy;
+    OcaFloat32 JitterMax;
+
+public:
+    OcaMediaClockRate(){};
+    virtual ~OcaMediaClockRate(){};
+};
+
+enum class OcaMediaClockType : OcaEnumItem
+{
+    None = 0,
+    Internal,
+    Network,
+    External
+};
+
 #endif // SAMPLINGTYPES_H
