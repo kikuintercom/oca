6ce45af97e01a1ab09aad57a53d10c7f9d85a594
Callum McLean <calmcl1@aol.com>
2015-04-15 02:01:42 +0000

    Added forward declarations for all Block and Matrix datatypes

--- include/types/blocktypes.h
+++ include/types/blocktypes.h
@@ -5,4 +5,103 @@
 BLOCK AND MATRIX DATATYPES
 ************************************************/
 
+class OcaBlockMember
+{
+    OcaObjectIdentification MemberObjectIdentification;
+    OcaONo ContainerObjectNumber;
+
+public:
+    OcaBlockMember(){};
+    virtual ~OcaBlockMember(){};
+};
+
+enum class OcaPortMode : OcaEnumItem
+{
+    Input = 1,
+    Output
+};
+
+class OcaPortID
+{
+    OcaPortMode Mode;
+    OcaUint16 Index;
+
+public:
+    OcaPortID(){};
+    virtual ~OcaPortID(){};
+};
+
+class OcaPort
+{
+    OcaONo Owner;
+    OcaPortID ID;
+    OcaString Name;
+
+public:
+    OcaPort(){};
+    virtual ~OcaPort(){};
+};
+
+class OcaSignalPath
+{
+    OcaPort SourcePort, SinkPort;
+
+public:
+    OcaSignalPath(){};
+    virtual ~OcaSignalPath(){};
+};
+
+typedef OcaUint32 OcaProtoONo;
+
+class OcaProtoMember
+{
+    OcaProtoONo ProtoMemberPONo;
+
+public:
+    OcaProtoMember(){};
+    virtual ~OcaProtoMember(){};
+};
+
+class OcaProtoObjectIdentification
+{
+    OcaProtoONo POno;
+    OcaClassIdentification ClassIdentification;
+
+public:
+    OcaProtoObjectIdentification(){};
+    virtual ~OcaProtoObjectIdentification(){};
+};
+
+class OcaProtoPortID
+{
+    OcaPortMode Mode;
+    OcaUint16 Index;
+
+public:
+    OcaProtoPortID(){};
+    virtual ~OcaProtoPortID(){};
+};
+
+class OcaProtoPort
+{
+    OcaProtoONo Owner;
+    OcaProtoPortID ProtoID;
+    OcaString Name;
+
+public:
+    OcaProtoPort(){};
+    virtual ~OcaProtoPort(){};
+};
+
+class OcaProtoSignalPath
+{
+    OcaProtoPort SourceProtoPort, SinkProtoPort;
+
+public:
+    OcaProtoSignalPath(){};
+    virtual ~OcaProtoSignalPath();
+};
+
+typedef OcaUint16 OcaMatrixCoordinate;
+
 #endif // BLOCKTYPES_H
